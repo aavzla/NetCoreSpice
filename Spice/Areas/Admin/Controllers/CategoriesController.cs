150cbca47c49fd9ec052327289c8afd7769fe8e4
AAvilan <avilan.andres.cem@gmail.com>
2019-11-29 15:50:23 -0500

    Create the Categories Controller and the Index and Create view along with partial views for the Create button and the Create and Back to List buttons. Also, modify the css to have some general classes, Add the Url for the icons in the project.

---
+++ Spice/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Categories.ToListAsync());
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+    }
+}
