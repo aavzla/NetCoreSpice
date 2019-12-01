6c83813f781bcdf96b396931a96d3dc5494efcc9
AAvilan <avilan.andres.cem@gmail.com>
2019-12-01 08:42:22 -0500

    Add SubCategories controller, the Index view, access from menu and Clean up the Include folders in the .csproj file.

---
+++ Spice/Areas/Admin/Controllers/SubCategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = await _db.SubCategories.Include(s => s.Category).ToListAsync();
+            return View(subCategories);
+        }
+    }
+}
