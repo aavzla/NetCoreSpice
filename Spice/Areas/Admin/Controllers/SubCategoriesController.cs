98d9f3b94213661ecb71183bedfa5296782980d1
AAvilan <avilan.andres.cem@gmail.com>
2019-12-01 19:47:45 -0500

    Add Sub Categories to the project with Crud operations. Add Extensions methods for parsing IEnumerables to SelectLists and Get Values from Properties by reflection. And a partial view for show messages to the user.

--- Spice/Areas/Admin/Controllers/SubCategoriesController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoriesController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -12,6 +15,11 @@ namespace Spice.Areas.Admin.Controllers
     public class SubCategoriesController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        //This was in the course, but we use it only to add a message into the viewModel, so a local variable is best.
+        //[TempData]
+        //public string StatusMessage { get; set; }
+
         public SubCategoriesController(ApplicationDbContext db)
         {
             _db = db;
@@ -22,6 +30,164 @@ namespace Spice.Areas.Admin.Controllers
         {
             var subCategories = await _db.SubCategories.Include(s => s.Category).ToListAsync();
             return View(subCategories);
+        }
+
+        //GET
+        public async Task<IActionResult> Create()
+        {
+            SubCategoriesCreateViewModel viewModel = new SubCategoriesCreateViewModel()
+            {
+                Categories = await _db.Categories.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryNameList = await _db.SubCategories.OrderBy(s => s.Name).Select(sb => sb.Name).Distinct().ToListAsync()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCategoriesCreateViewModel viewModel)
+        {
+            string statusMessage = null;
+            if (ModelState.IsValid)
+            {
+                var subCategoriesInCategory = _db.SubCategories.Include(s => s.Category).Where(sc => sc.Name == viewModel.SubCategory.Name && sc.CategoryId == viewModel.SubCategory.CategoryId);
+                if (subCategoriesInCategory.Any())
+                {
+                    //Error
+                    statusMessage = $"Error : Sub Category {viewModel.SubCategory.Name} exists under {subCategoriesInCategory.First().Category.Name} Category. Please verify.";
+                }
+                else
+                {
+                    _db.SubCategories.Add(viewModel.SubCategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            SubCategoriesCreateViewModel vm = new SubCategoriesCreateViewModel()
+            {
+                Categories = await _db.Categories.ToListAsync(),
+                SubCategory = viewModel.SubCategory,
+                SubCategoryNameList = await _db.SubCategories.OrderBy(s => s.Name).Select(sb => sb.Name).Distinct().ToListAsync(),
+                StatusMessage = statusMessage
+            };
+
+            return View(vm);
+        }
+        
+        // JSON
+        public async Task<IActionResult> GetSubCategory(int id)
+        {
+            List<SubCategory> subCategories = await _db.SubCategories.Where(s => s.CategoryId == id).ToListAsync();
+            return Json(new SelectList(subCategories, "Id", "Name"));
+        }
+
+        //GET
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                var subCategory = await _db.SubCategories.SingleOrDefaultAsync(s => s.Id == id);
+
+                if (subCategory != null)
+                {
+                    SubCategoriesCreateViewModel viewModel = new SubCategoriesCreateViewModel()
+                    {
+                        Categories = await _db.Categories.ToListAsync(),
+                        SubCategory = subCategory,
+                        SubCategoryNameList = await _db.SubCategories.OrderBy(s => s.Name).Select(sb => sb.Name).Distinct().ToListAsync()
+                    };
+
+                    return View(viewModel);
+                }
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SubCategoriesCreateViewModel viewModel)
+        {
+            string statusMessage = null;
+            if (ModelState.IsValid)
+            {
+                var subCategoriesInCategory = _db.SubCategories.Include(s => s.Category).Where(sc => sc.Name == viewModel.SubCategory.Name && sc.CategoryId == viewModel.SubCategory.CategoryId);
+                if (subCategoriesInCategory.Any())
+                {
+                    //Error
+                    statusMessage = $"Error : Sub Category {viewModel.SubCategory.Name} exists under {subCategoriesInCategory.First().Category.Name} Category. Please verify.";
+                }
+                else
+                {
+                    var subCategory = await _db.SubCategories.FindAsync(viewModel.SubCategory.Id);
+                    subCategory.Name = viewModel.SubCategory.Name;
+
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            SubCategoriesCreateViewModel vm = new SubCategoriesCreateViewModel()
+            {
+                Categories = await _db.Categories.ToListAsync(),
+                SubCategory = viewModel.SubCategory,
+                SubCategoryNameList = await _db.SubCategories.OrderBy(s => s.Name).Select(sb => sb.Name).Distinct().ToListAsync(),
+                StatusMessage = statusMessage
+            };
+
+            return View(vm);
+        }
+
+        //GET
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                var subCategory = await _db.SubCategories.Include(s=> s.Category).SingleOrDefaultAsync(s => s.Id == id);
+
+                if (subCategory != null)
+                {
+                    return View(subCategory);
+                }
+            }
+
+            return NotFound();
+        }
+
+        //GET
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id.HasValue)
+            {
+                var subCategory = await _db.SubCategories.Include(s => s.Category).SingleOrDefaultAsync(s => s.Id == id);
+
+                if (subCategory != null)
+                {
+                    return View(subCategory);
+                }
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var subCategory = await _db.SubCategories.FindAsync(id);
+                if (subCategory != null)
+                {
+                    _db.SubCategories.Remove(subCategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return NotFound();
         }
     }
 }
---
+++ Spice/Models/ViewModels/SubCategoriesCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Spice.Models.ViewModels
+{
+    public class SubCategoriesCreateViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public List<string> SubCategoryNameList { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
